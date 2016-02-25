235ebed1dae91f6e5422cb31fffb82181da49634
戴鑫通 <xintong.dai@wenba100.com>
2016-02-25 13:19:40 +0800

    update
    Signed-off-by: 戴鑫通 <xintong.dai@wenba100.com>

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,39 @@
+class Users::SessionsController < Devise::SessionsController
+# before_filter :configure_sign_in_params, only: [:create]
+  prepend_before_filter { request.env["devise.mapping"] = Devise.mappings[:user] }
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  def destroy
+    DistSession.where(:session_id => cookies[:dist_session_id]).delete_all if cookies[:dist_session_id]
+    cookies.delete :dist_session_id, :domain => Settings.shared_cookies_domain
+    super
+  end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  # end
+
+  def after_sign_in_path_for(resource_or_scope)
+    dist_session = DistSession.find_or_create_by :session_id => session.id
+    dist_session.update :data => {:user_id => resource.id}.to_json
+    cookies[:dist_session_id] = {:value => dist_session.session_id, :domain => Settings.shared_cookies_domain}
+    return_to = params[:target] ? params[:target] : Settings.hosts.web.url
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    return_to = params[:target] ? params[:target] : Settings.hosts.web.url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'welcome#index'
-  get 'logout' => 'logout#index'
+  devise_for :users, :controllers => {:sessions => 'users/sessions'}
+  devise_scope :users do
+    get 'users/sign_out' => 'users/sessions#destroy'
+  end
+  # root 'welcome#index'
+  # get 'logout' => 'logout#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
